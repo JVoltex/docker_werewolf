7b3ef4e665a2b133cf58809598c58fee0c26005b
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2021-01-03 18:46:08 +0900

    routine

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ const io = require("socket.io")(server)
 app.use(express.static("../client/out"))
 
 const members = []
+const n_member = 5
+const jobs = {
+  werewolf: 1,
+  fourtune_teller: 1,
+  hunter: 1,
+  shaman: 1,
+  other: 1
+}
 
 io.on("connection", (socket) => {
   console.log("connected")
@@ -14,11 +22,11 @@ io.on("connection", (socket) => {
     io.emit("serverMessage", msg)
   })
   socket.on("clientMemberJoin", (msg) => {
-    members.push(msg)
+    members.push({socket: socket, name: msg})
     console.log(`clientMemberJoin: ${msg}`)
-    io.emit("serverMemberJoin", members)
-    console.log(members)
+    io.emit("serverMemberJoin", members.map((x) => x.name))
     io.emit("serverMessage", `${msg} が入室しました`)
+    // memo: removeLister
   })
 })
 
