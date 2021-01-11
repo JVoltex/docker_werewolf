73242f86279b4c2fec89d9928389c9424ec72522
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2021-01-11 16:12:24 +0900

    fix prompt

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ const { inputNonNegativeInteger } = require("./lib/utils");
 
 const playGameOnce = async (jobs, port, staticDir="../client/out/") => {
   const assign = await inputAssign(jobs)
-  const timeLimit = await inputNonNegativeInteger("相談時間は何秒？")
+  const timeLimit = await inputNonNegativeInteger("相談時間は何秒？：")
   console.log("設定完了です。ブラウザからアクセスしてください。")
   const server = new GameServer(staticDir, port)
   server.start();
